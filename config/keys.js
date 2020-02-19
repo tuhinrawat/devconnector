9b8ca01168747034439e40e0f282b5c6e7ed6f6d
Tuhin Rawat <tuhinrawat@gmail.com>
2020-02-20 02:42:42 +0530

    Initial express server with route file

---
+++ config/keys.js
@@ -0,0 +1,4 @@
+module.exports = {
+  mongoURI:
+    "mongodb+srv://tuhin:<root>@devconnector-n9ww7.mongodb.net/test?retryWrites=true&w=majority"
+};
---
+++ routes/api/profile.js
@@ -0,0 +1,8 @@
+const express = require("express");
+const router = express.Router();
+
+// @route   GET api/profile/test
+// @desc    Test post route
+// @access  public
+router.get("/test", (req, res) => res.json({ msg: "Profile Works" }));
+module.exports = router;
